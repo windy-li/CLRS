5bf8115de4b979cb464a958b8cb4e9e2693c6f77
windy-li <2249975638@qq.com>
2020-10-21 09:48:46 +0800

    .

--- part2/p089_heap_sort.cc
+++ part2/p089_heap_sort.cc
@@ -65,3 +65,14 @@ class MaxHeap {
 
   int Right(int i) { return 2 * i + 2; }
 };
+
+int main() {
+  std::vector<int> a = {5, 2, 4, 6, 1, 3};
+  MaxHeap heap;
+  heap.Sort(a);
+  clrs::PrintVector(a);
+}
+
+/*
+ * 1 2 3 4 5 6
+ */
