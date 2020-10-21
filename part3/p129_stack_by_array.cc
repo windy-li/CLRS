bcd2d1246f109b22df6de6f2661035bd262525aa
windy-li <2249975638@qq.com>
2020-10-21 11:36:23 +0800

    .

--- part3/p129_stack_by_array.cc
+++ part3/p129_stack_by_array.cc
@@ -30,3 +30,17 @@ class Stack {
   int capacity_;
   int top_;
 };
+
+int main() {
+  Stack s(7);
+  s.Push(15);
+  s.Push(6);
+  s.Push(2);
+  s.Push(9);
+  s.Push(17);
+  s.Push(3);
+  auto [ret, err] = s.Pop();
+  if (!err) {
+    std::cout << ret << std::endl;
+  }
+}
--- part3/p132_doubly_linked_list.cc
+++ part3/p132_doubly_linked_list.cc
@@ -67,3 +67,57 @@ class LinkedList {
  private:
   Node* head_;
 };
+
+void PrintNode(Node* node) {
+  if (node == nullptr) {
+    std::cout << "null" << std::endl;
+    return;
+  }
+  std::cout << "{ key: " << node->key;
+  if (node->prev != nullptr) {
+    std::cout << ", prev: " << node->prev->key;
+  }
+  if (node->next != nullptr) {
+    std::cout << ", next: " << node->next->key;
+  }
+  std::cout << " }";
+}
+
+void PrintLinkedList(LinkedList* l) {
+  Node* node = l->head();
+  while (node != nullptr) {
+    std::cout << node->key;
+    if (node->next != nullptr) {
+      std::cout << " -> ";
+    }
+    node = node->next;
+  }
+}
+
+void TestLinkedList() {
+  LinkedList l;
+  l.PushFront(1);
+  l.PushFront(4);
+  l.PushFront(16);
+  l.PushFront(9);
+  l.PushFront(25);
+  Node* x1 = l.Search(4);
+  if (x1 != nullptr) {
+    PrintNode(x1);
+  } else {
+    std::cout << "null" << std::endl;
+  }
+  PrintLinkedList(&l);
+  l.Reverse();
+  PrintLinkedList(&l);
+  l.RemoveKey(4);
+  Node* x2 = l.Search(4);
+  if (x2 != nullptr) {
+    PrintNode(x2);
+  } else {
+    std::cout << "null" << std::endl;
+  }
+  PrintLinkedList(&l);
+}
+
+int main() { TestLinkedList(); }
