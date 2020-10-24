db5af2dbcbc32d696c2574ddd61d0d990e9a839f
windy-li <2249975638@qq.com>
2020-10-24 13:39:07 +0800

    .

--- part1/p070_permute_by_sorting.cc
+++ part1/p070_permute_by_sorting.cc
@@ -2,19 +2,19 @@
 
 class Solution {
  public:
-  void PermuteBySorting(std::vector<int>& a) {
-    int n = a.size();
+  void PermuteBySorting(std::vector<int>& nums) {
+    int n = nums.size();
     std::vector<int> p(n);
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < n; ++i) {
       p[i] = clrs::RandomInt(0, n * n * n);
     }
-    SortForPermute(a, p);
+    SortForPermute(nums, p);
   }
 
  private:
-  void SortForPermute(std::vector<int>& a, std::vector<int>& p) {
-    int n = a.size();
-    for (int j = 1; j < n; j++) {
+  void SortForPermute(std::vector<int>& nums, std::vector<int>& p) {
+    int n = nums.size();
+    for (int j = 1; j < n; ++j) {
       int key = p[j];
       int i = j - 1;
       while (i >= 0 && p[i] > key) {
@@ -22,7 +22,7 @@ class Solution {
         --i;
       }
       p[i + 1] = key;
-      std::swap(a[i + 1], a[j]);
+      std::swap(nums[i + 1], nums[j]);
     }
   }
 };
