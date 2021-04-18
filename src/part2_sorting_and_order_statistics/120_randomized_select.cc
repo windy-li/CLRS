9fb1e3ffc7a42b17bb25a81cbe142648621a8efb
xiao-tudou <2249975638@qq.com>
2021-04-18 13:49:46 +0800

    .

---
+++ src/part2_sorting_and_order_statistics/120_randomized_select.cc
@@ -0,0 +1,71 @@
+#include "clrs.h"
+
+class Solution {
+ public:
+  int Recursive(std::vector<int>& nums, int p, int r, int i) {
+    if (p == r) {
+      return nums[p];
+    }
+    int q = RandomizedPartition(nums, p, r);
+    int k = q - p + 1;
+    if (i == k) {
+      return nums[q];
+    } else if (i < k) {
+      return Recursive(nums, p, q - 1, i);
+    } else {
+      return Recursive(nums, q + 1, r, i - k);
+    }
+  }
+
+  int Iterative(std::vector<int>& nums, int p, int r, int i) {
+    while (p < r) {
+      int q = RandomizedPartition(nums, p, r);
+      int k = q - p + 1;
+      if (i == k) {
+        return nums[q];
+      } else if (i < k) {
+        r = q - 1;
+      } else {
+        p = q + 1;
+        i -= k;
+      }
+    }
+    return nums[p];
+  }
+
+ private:
+  int RandomizedPartition(std::vector<int>& nums, int p, int r) {
+    int i = clrs::RandomInt(p, r + 1);
+    std::swap(nums[i], nums[r]);
+    return Partition(nums, p, r);
+  }
+
+  int Partition(std::vector<int>& nums, int p, int r) {
+    int i = p - 1;
+    for (int j = p; j < r; ++j) {
+      if (nums[j] <= nums[r]) {
+        i++;
+        std::swap(nums[i], nums[j]);
+      }
+    }
+    std::swap(nums[i + 1], nums[r]);
+    return i + 1;
+  }
+};
+
+void TestRecursive() {
+  std::vector<int> nums = {2, 8, 7, 1, 3, 5, 6, 4};
+  Solution s;
+  std::cout << s.Recursive(nums, 0, nums.size() - 1, 1) << std::endl;
+}
+
+void TestIterative() {
+  std::vector<int> nums = {2, 8, 7, 1, 3, 5, 6, 4};
+  Solution s;
+  std::cout << s.Iterative(nums, 0, nums.size() - 1, 1) << std::endl;
+}
+
+int main() {
+  TestRecursive();
+  TestIterative();
+}
