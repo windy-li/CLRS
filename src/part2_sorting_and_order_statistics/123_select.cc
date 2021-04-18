8579ddb492949c5ced7d69a4900e07a055a92fab
xiao-tudou <2249975638@qq.com>
2021-04-18 18:55:32 +0800

    .

--- src/part2_sorting_and_order_statistics/123_select.cc
+++ src/part2_sorting_and_order_statistics/123_select.cc
@@ -2,27 +2,27 @@
 
 class Solution {
  public:
-  int Select(std::vector<int>& nums, int p, int r, int i) {
-    if (p == r) {
-      return nums[p];
+  int Select(std::vector<int>& nums, int left_bound, int right_bound, int i) {
+    if (left_bound == right_bound) {
+      return nums[left_bound];
     }
-    int n = r - p + 1;
+    int n = right_bound - left_bound + 1;
     int g = (n + 4) / 5;
     std::vector<int> medians(g);
     for (int j = 0; j < g; ++j) {
-      int start = p + j * 5;
-      int end = (start + 4 > r) ? r : (start + 4);
+      int start = left_bound + j * 5;
+      int end = (start + 4 > right_bound) ? right_bound : (start + 4);
       medians[j] = GetMedian(nums, start, end);
     }
     int med_of_med = Select(medians, 0, g - 1, (g + 1) / 2);
-    int q = Partition(nums, p, r, med_of_med);
-    int k = q - p + 1;
+    int q = Partition(nums, left_bound, right_bound, med_of_med);
+    int k = q - left_bound + 1;
     if (i == k) {
       return nums[q];
     } else if (i < k) {
-      return Select(nums, p, q - 1, i);
+      return Select(nums, left_bound, q - 1, i);
     } else {
-      return Select(nums, q + 1, r, i - k);
+      return Select(nums, q + 1, right_bound, i - k);
     }
   }
 
