0d3fe4e66efeef71bcd2cbd0611369372d9d83b2
xiao-tudou <2249975638@qq.com>
2021-04-20 17:36:31 +0800

    .

--- src/part4_advanced_design_and_analysis_techniques/211_matrix_chain_multiplication.cc
+++ src/part4_advanced_design_and_analysis_techniques/211_matrix_chain_multiplication.cc
@@ -117,10 +117,10 @@ class Solution {
 void TestMatrixMultiply() {
   std::vector<std::vector<int>> a = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 1, 2, 3}};
   std::vector<std::vector<int>> b = {{1, 2}, {3, 4}, {5, 6}, {7, 8}};
-  int a_row = a.size();
-  int b_col = b[0].size();
   Solution s;
   std::vector<std::vector<int>> c = s.MatrixMultiply(a, b);
+  int a_row = a.size();
+  int b_col = b[0].size();
   for (int i = 0; i < a_row; ++i) {
     for (int j = 0; j < b_col; ++j) {
       std::cout << c[i][j] << " ";
