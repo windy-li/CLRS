df76d98dbd0c3f01c308902e897306ce058172d1
xiao-tudou <2249975638@qq.com>
2021-05-10 10:06:28 +0800

    .

--- src/part4_advanced_design_and_analysis_techniques/246_huffman.cc
+++ src/part4_advanced_design_and_analysis_techniques/246_huffman.cc
@@ -18,4 +18,50 @@ class Node {
 
 class Solution {
  public:
+  Node* BuildTree(std::vector<char>& keys, std::vector<int>& weights) {
+    int n = keys.size();
+    std::vector<Node*> nodes(n, new Node());
+    for (int i = 0; i < n; ++i) {
+      nodes[i] = new Node(keys[i], weights[i]);
+    }
+    std::priority_queue<Node*> queue;
+    for (Node* node : nodes) {
+      queue.push(node);
+    }
+    for (int i = 0; i < n - 1; ++i) {
+      Node* z = new Node();
+      Node* x = queue.top();
+      queue.pop();
+      Node* y = queue.top();
+      queue.pop();
+      z->left_ = x;
+      z->right_ = y;
+      z->weight_ = x->weight_ + y->weight_;
+      queue.push(z);
+    }
+    Node* ret = queue.top();
+    queue.pop();
+    return ret;
+  }
+
+  void PrintCode(Node* node, const std::string& s) {
+    if (node != nullptr) {
+      PrintCode(node->left_, s + "0");
+      if (node->left_ == nullptr && node->right_ == nullptr) {
+        std::cout << node->key_ << ": " << s;
+      }
+      PrintCode(node->right_, s + "1");
+    }
+  }
 };
+
+void TestHuffman() {
+  Solution solution;
+  std::vector<char> keys = {'a', 'b', 'c', 'd', 'e', 'f'};
+  std::vector<int> weights = {45, 13, 12, 16, 9, 5};
+  Node* root = solution.BuildTree(keys, weights);
+  std::cout << root << std::endl;
+  solution.PrintCode(root, "");
+}
+
+int main() { TestHuffman(); }
