8b73956fa2c8466b405aaac3a33a752f147b3eed
xiao-tudou <2249975638@qq.com>
2021-05-12 17:29:13 +0800

    .

--- src/part6_graph_algorithms/344_breadth_first_search.cc
+++ src/part6_graph_algorithms/344_breadth_first_search.cc
@@ -1,12 +1,50 @@
 #include "clrs.h"
 
-class Vertex {};
+struct Vertex {
+  bool visited;
+  int d;
+  Vertex* pre;
+  int id;
+};
 
-class Edge {};
+struct Edge {
+  int other(int id) { return 0; }
+};
 
-class Graph {};
+struct Graph {
+  int V;
+  std::vector<Vertex*> vertices;
+  std::vector<std::list<Edge*>> adj;
+};
 
 class Solution {
  public:
-  void BreadthFirstSearch(Graph* graph, int src_id) {}
+  void BreadthFirstSearch(Graph* graph, int src_id) {
+    int V = graph->V;
+    for (int i = 0; i < V; ++i) {
+      Vertex* u = graph->vertices[i];
+      u->visited = false;
+      u->d = std::numeric_limits<int>::max();
+      u->pre = nullptr;
+    }
+    Vertex* src = graph->vertices[src_id];
+    src->visited = true;
+    src->d = 0;
+    std::queue<Vertex*> q;
+    q.push(src);
+    while (!q.empty()) {
+      Vertex* u = q.front();
+      q.pop();
+      for (auto* e : graph->adj[u->id]) {
+        Vertex* v = graph->vertices[e->other(u->id)];
+        if (!v->visited) {
+          v->visited = true;
+          v->d = u->d + 1;
+          v->pre = u;
+          q.push(v);
+        }
+      }
+      u->visited = true;
+    }
+  }
 };
