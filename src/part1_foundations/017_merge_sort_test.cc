d63c0f8cdc84075c5780ca8c5914fd7b0a2dcb7b
xiao-tudou <2249975638@qq.com>
2021-06-03 20:03:24 +0800

    .

--- src/part1_foundations/017_merge_sort_test.cc
+++ src/part1_foundations/017_merge_sort_test.cc
@@ -2,39 +2,39 @@
 
 class Solution {
  public:
-  void MergeSort(std::vector<int>& nums, int left_bound, int right_bound) {
-    if (left_bound >= right_bound) {
+  void MergeSort(std::vector<int>& nums, int p, int r) {
+    if (p >= r) {
       return;
     }
-    int middle = (left_bound + right_bound) / 2;
-    MergeSort(nums, left_bound, middle);
-    MergeSort(nums, middle + 1, right_bound);
-    Merge(nums, left_bound, middle, right_bound);
+    int q = (p + r) / 2;
+    MergeSort(nums, p, q);
+    MergeSort(nums, q + 1, r);
+    Merge(nums, p, q, r);
   }
 
-  void MergeSortBySentinels(std::vector<int>& nums, int left_bound, int right_bound) {
-    if (left_bound >= right_bound) {
+  void MergeSortBySentinels(std::vector<int>& nums, int p, int r) {
+    if (p >= r) {
       return;
     }
-    int middle = (left_bound + right_bound) / 2;
-    MergeSortBySentinels(nums, left_bound, middle);
-    MergeSortBySentinels(nums, middle + 1, right_bound);
-    MergeBySentinels(nums, left_bound, middle, right_bound);
+    int q = (p + r) / 2;
+    MergeSortBySentinels(nums, p, q);
+    MergeSortBySentinels(nums, q + 1, r);
+    MergeBySentinels(nums, p, q, r);
   }
 
  private:
-  void Merge(std::vector<int>& nums, int left_bound, int middle, int right_bound) {
-    int n1 = middle - left_bound + 1;
-    int n2 = right_bound - middle;
+  void Merge(std::vector<int>& nums, int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
     std::vector<int> left(n1);
     std::vector<int> right(n2);
     for (int i = 0; i < n1; ++i) {
-      left[i] = nums[left_bound + i];
+      left[i] = nums[p + i];
     }
     for (int i = 0; i < n2; ++i) {
-      right[i] = nums[middle + 1 + i];
+      right[i] = nums[q + 1 + i];
     }
-    int i = 0, j = 0, k = left_bound;
+    int i = 0, j = 0, k = p;
     while (i < n1 && j < n2) {
       if (left[i] <= right[j]) {
         nums[k++] = left[i++];
@@ -50,21 +50,21 @@ class Solution {
     }
   }
 
-  void MergeBySentinels(std::vector<int>& nums, int left_bound, int middle, int right_bound) {
-    int n1 = middle - left_bound + 1;
-    int n2 = right_bound - middle;
+  void MergeBySentinels(std::vector<int>& nums, int p, int q, int r) {
+    int n1 = q - p + 1;
+    int n2 = r - q;
     std::vector<int> left(n1 + 1);
     std::vector<int> right(n2 + 1);
     for (int i = 0; i < n1; ++i) {
-      left[i] = nums[left_bound + i];
+      left[i] = nums[p + i];
     }
     left[n1] = std::numeric_limits<int>::max();
     for (int i = 0; i < n2; ++i) {
-      right[i] = nums[middle + 1 + i];
+      right[i] = nums[q + 1 + i];
     }
     right[n2] = std::numeric_limits<int>::max();
     int i = 0, j = 0;
-    for (int k = left_bound; k <= right_bound; ++k) {
+    for (int k = p; k <= r; ++k) {
       if (left[i] <= right[j]) {
         nums[k] = left[i++];
       } else {
