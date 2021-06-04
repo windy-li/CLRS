0b832f8bc46eda7b6ffe0df3c4a7c19dbcda50a8
xiao-tudou <2249975638@qq.com>
2021-06-04 14:05:17 +0800

    .

--- src/part1_foundations/016_selection_sort_test.cc
+++ src/part1_foundations/016_selection_sort_test.cc
@@ -6,7 +6,7 @@
 Solution solution;
 
 TEST(SelectionSortTest, SelectionSort) {
-  std::vector<int> nums = clrs::RandomVector();
+  std::vector<int> nums = clrs::Random::Vector();
   ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end()));
   solution.SelectionSort(nums);
   ASSERT_TRUE(std::is_sorted(nums.begin(), nums.end()));
