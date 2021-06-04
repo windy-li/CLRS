0d5ca9aaf1f2dbff7f3ce2350b49103f4acbb094
xiao-tudou <2249975638@qq.com>
2021-06-04 10:33:52 +0800

    .

--- src/clrs.h
+++ src/clrs.h
@@ -19,6 +19,51 @@
 
 namespace clrs {
 
+class Random {
+ public:
+  static int Get(int left, int right) {
+    std::uniform_int_distribution<int> distribution(left, right - 1);
+    std::default_random_engine engine = GetEngine();
+    return distribution(engine);
+  }
+
+  static double Get() {
+    std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    std::default_random_engine engine = GetEngine();
+    return distribution(engine);
+  }
+
+  static std::vector<int> Vector() {
+    int n = 1000;
+    int low = 0;
+    int high = 100;
+    std::vector<int> vec(n);
+    for (int i = 0; i < n; i++) {
+      vec[i] = Get(low, high);
+    }
+    return vec;
+  }
+
+ private:
+  static bool initialized_;
+  static std::default_random_engine engine_;
+
+  static std::default_random_engine GetEngine();
+};
+
+bool Random::initialized_ = false;
+std::default_random_engine Random::engine_;
+
+std::default_random_engine Random::GetEngine() {
+  if (initialized_) {
+    return engine_;
+  }
+  initialized_ = true;
+  std::random_device device;
+  engine_ = std::default_random_engine(device());
+  return engine_;
+}
+
 template <typename T>
 void PrintVector(std::vector<T>& vec) {
   int n = vec.size();
@@ -32,32 +77,7 @@ void PrintVector(std::vector<T>& vec) {
   std::cout << "]" << std::endl;
 }
 
-int RandomInt(int left_bound_included, int right_bound_excluded) {
-  std::random_device rd;
-  auto gen = std::default_random_engine(rd());
-  std::uniform_int_distribution<int> dis(left_bound_included, right_bound_excluded - 1);
-  return dis(gen);
-}
-
-double Random() {
-  std::random_device rd;
-  auto gen = std::default_random_engine(rd());
-  std::uniform_real_distribution<double> dis(0.0, 1.0);
-  return dis(gen);
-}
-
 void PrintBorder() { std::cout << "------------------------------" << std::endl; }
-
-std::vector<int> RandomVector() {
-  int n = 1000;
-  int low = 0;
-  int high = 100;
-  std::vector<int> vec(n);
-  for (int i = 0; i < n; ++i) {
-    vec[i] = RandomInt(low, high);
-  }
-  return vec;
-}
 
 }  // namespace clrs
 
--- src/part1_foundations/010_insertion_sort_test.cc
+++ src/part1_foundations/010_insertion_sort_test.cc
@@ -6,14 +6,14 @@
 Solution solution;
 
 TEST(InsertionSortTest, InsertionSort) {
-  std::vector<int> nums = clrs::RandomVector();
+  std::vector<int> nums = clrs::Random::Vector();
   ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end()));
   solution.InsertionSort(nums);
   ASSERT_TRUE(std::is_sorted(nums.begin(), nums.end()));
 }
 
 TEST(InsertionSortTest, RecursiveInsertionSort) {
-  std::vector<int> nums = clrs::RandomVector();
+  std::vector<int> nums = clrs::Random::Vector();
   ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end()));
   int n = nums.size();
   solution.RecursiveInsertionSort(nums, n - 1);
@@ -21,14 +21,14 @@ TEST(InsertionSortTest, RecursiveInsertionSort) {
 }
 
 TEST(InsertionSortTest, NonIncreasingInsertionSort) {
-  std::vector<int> nums = clrs::RandomVector();
+  std::vector<int> nums = clrs::Random::Vector();
   ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end(), [](int a, int b) { return b < a; }));
   solution.NonIncreasingInsertionSort(nums);
   ASSERT_TRUE(std::is_sorted(nums.begin(), nums.end(), [](int a, int b) { return b < a; }));
 }
 
 TEST(InsertionSortTest, BinaryInsertionSort) {
-  std::vector<int> nums = clrs::RandomVector();
+  std::vector<int> nums = clrs::Random::Vector();
   ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end()));
   solution.BinaryInsertionSort(nums);
   ASSERT_TRUE(std::is_sorted(nums.begin(), nums.end()));
