e1a9c5e8ab356532051a966e8402b131331e2021
xiao-tudou <2249975638@qq.com>
2021-06-09 16:02:49 +0800

    .

--- src/part1_foundations/012_binary_integer_adding_test.cc
+++ src/part1_foundations/012_binary_integer_adding_test.cc
@@ -5,9 +5,9 @@ Solution solution;
 TEST(BinaryIntegerAddingTest, BinaryIntegerAdding) {
   std::vector<int> a = {1, 0, 1, 1, 1, 0};
   std::vector<int> b = {0, 1, 1, 1, 0, 0};
-  std::vector<int> x = {1, 0, 0, 1, 0, 1, 0};
-  std::vector<int> y = solution.BinaryIntegerAdding(a, b);
-  for (int i = 0; i < x.size(); ++i) {
-    EXPECT_EQ(x[i], y[i]) << "vectors x and y differ at index " << i;
+  std::vector<int> c = {1, 0, 0, 1, 0, 1, 0};
+  std::vector<int> d = solution.BinaryIntegerAdding(a, b);
+  for (int i = 0; i < c.size(); ++i) {
+    EXPECT_EQ(c[i], d[i]) << "vectors c and d differ at index " << i;
   }
 }
