398b77a56657afa68b50b7a2a1291e3afc72eaf4
xiao-tudou <2249975638@qq.com>
2021-08-03 20:36:06 +0800

    .

--- src/part6_graph_algorithms/344_breadth_first_search_test.cc
+++ src/part6_graph_algorithms/344_breadth_first_search_test.cc
@@ -3,19 +3,19 @@
 Solution solution;
 
 TEST(BreadthFirstSearchTest, TestBreadthFirstSearch) {
-  auto* graph = new Graph(7);
-  graph->AddEdge(0, 1);
-  graph->AddEdge(0, 2);
-  graph->AddEdge(1, 3);
-  graph->AddEdge(2, 3);
-  graph->AddEdge(3, 4);
-  graph->AddEdge(3, 5);
-  solution.BreadthFirstSearch(graph, 0);
-  graph_util::PrintPath(graph->vertices[0], graph->vertices[5]);
+  Graph graph(7);
+  graph.AddEdge(0, 1);
+  graph.AddEdge(0, 2);
+  graph.AddEdge(1, 3);
+  graph.AddEdge(2, 3);
+  graph.AddEdge(3, 4);
+  graph.AddEdge(3, 5);
+  solution.BreadthFirstSearch(&graph, 0);
+  graph_util::PrintPath(graph.vertices[0], graph.vertices[5]);
   std::cout << std::endl;
-  graph_util::PrintPath(graph->vertices[0], graph->vertices[6]);
+  graph_util::PrintPath(graph.vertices[0], graph.vertices[6]);
   std::cout << std::endl;
-  std::cout << Graph::ToString(graph) << std::endl;
+  std::cout << Graph::ToString(&graph) << std::endl;
 }
 
 /*
--- src/part6_graph_algorithms/350_depth_first_search_test.cc
+++ src/part6_graph_algorithms/350_depth_first_search_test.cc
@@ -1,15 +1,15 @@
 #include "part6_graph_algorithms/344_depth_first_search.h"
 
 TEST(DepthFirstSearchTest, DepthFirstSearch) {
-  auto* graph = new Graph(6);
-  graph->AddEdge(0, 1);
-  graph->AddEdge(0, 2);
-  graph->AddEdge(1, 3);
-  graph->AddEdge(2, 3);
-  graph->AddEdge(3, 4);
-  graph->AddEdge(3, 5);
+  Graph graph(6);
+  graph.AddEdge(0, 1);
+  graph.AddEdge(0, 2);
+  graph.AddEdge(1, 3);
+  graph.AddEdge(2, 3);
+  graph.AddEdge(3, 4);
+  graph.AddEdge(3, 5);
   Solution solution;
-  solution.DFS(graph);
+  solution.DFS(&graph);
 }
 
 RUN_TESTS()
