fbd33e65f10adf719eaaa6dd7e23c34923db6d99
mitoli <2249975638@qq.com>
2021-08-14 13:31:17 +0800

    .

--- src/part2_sorting_and_order_statistics/111_radix_sort.h
+++ src/part2_sorting_and_order_statistics/111_radix_sort.h
@@ -1,4 +1,51 @@
 #ifndef CLRS_SRC_PART2_SORTING_AND_ORDER_STATISTICS_111_RADIX_SORT_H_
 #define CLRS_SRC_PART2_SORTING_AND_ORDER_STATISTICS_111_RADIX_SORT_H_
 
+#include "clrs.h"
+
+class Solution {
+ public:
+  void RadixSort(std::vector<int>& nums) {
+    // Find the maximum number to know number of digits
+    int max = *std::max_element(nums.begin(), nums.end());
+
+    // Do counting sort for every digit. Note that instead
+    // of passing digit number, exp is passed. exp is 10^i
+    // where i is current digit number
+    for (int exp = 1; max / exp > 0; exp *= 10) {
+      CountingSort(nums, exp);
+    }
+  }
+
+ private:
+  void CountingSort(std::vector<int>& nums, int exp) {
+    int n = nums.size();
+    int k = 9;
+    int count[k + 1];
+    int output[k + 1];
+    for (int i = 0; i <= k; ++i) {
+      count[i] = 0;
+    }
+    for (int i = 0; i < n; ++i) {
+      count[(nums[i] / exp) % 10]++;
+    }
+    // Change count[i] so that count[i] now contains
+    // actual position of this digit in output[]
+    for (int i = 1; i <= k; ++i) {
+      count[i] += count[i - 1];
+    }
+    // Build the output array
+    for (int i = n - 1; i >= 0; --i) {
+      int x = (nums[i] / exp) % 10;
+      output[count[x] - 1] = nums[i];
+      count[x]--;
+    }
+    // Copy the output array to nums[], so that nums[] now
+    // contains sorted numbers according to current digit
+    for (int i = 0; i < n; ++i) {
+      nums[i] = output[i];
+    }
+  }
+};
+
 #endif  // CLRS_SRC_PART2_SORTING_AND_ORDER_STATISTICS_111_RADIX_SORT_H_
--- src/part2_sorting_and_order_statistics/111_radix_sort_test.cc
+++ src/part2_sorting_and_order_statistics/111_radix_sort_test.cc
@@ -1,59 +1,15 @@
-#include "clrs.h"
+#include "part2_sorting_and_order_statistics/111_radix_sort.h"
 
-class Solution {
- public:
-  void RadixSort(std::vector<int>& nums) {
-    // Find the maximum number to know number of digits
-    int max = *std::max_element(nums.begin(), nums.end());
+Solution solution;
 
-    // Do counting sort for every digit. Note that instead
-    // of passing digit number, exp is passed. exp is 10^i
-    // where i is current digit number
-    for (int exp = 1; max / exp > 0; exp *= 10) {
-      CountingSort(nums, exp);
-    }
-  }
-
- private:
-  void CountingSort(std::vector<int>& nums, int exp) {
-    int n = nums.size();
-    int k = 9;
-    int count[k + 1];
-    int output[k + 1];
-    for (int i = 0; i <= k; ++i) {
-      count[i] = 0;
-    }
-    for (int i = 0; i < n; ++i) {
-      count[(nums[i] / exp) % 10]++;
-    }
-    // Change count[i] so that count[i] now contains
-    // actual position of this digit in output[]
-    for (int i = 1; i <= k; ++i) {
-      count[i] += count[i - 1];
-    }
-    // Build the output array
-    for (int i = n - 1; i >= 0; --i) {
-      int x = (nums[i] / exp) % 10;
-      output[count[x] - 1] = nums[i];
-      count[x]--;
-    }
-    // Copy the output array to nums[], so that nums[] now
-    // contains sorted numbers according to current digit
-    for (int i = 0; i < n; ++i) {
-      nums[i] = output[i];
-    }
-  }
-};
-
-void TestRadixSort() {
+TEST(RadixSortTest, TestRadixSort) {
   std::vector<int> nums = {73, 22, 93, 43, 55, 14, 28, 65, 39, 81};
-  Solution s;
-  s.RadixSort(nums);
+  ASSERT_FALSE(std::is_sorted(nums.begin(), nums.end()));
+  solution.RadixSort(nums);
+  ASSERT_TRUE(std::is_sorted(nums.begin(), nums.end()));
 }
 
-int main() {
-  TestRadixSort();
-}
+RUN_TESTS()
 
 /*
  * 81 22 73 93 43 14 55 65 28 39
